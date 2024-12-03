c47a659d1ef80368cf6ea1bfb2b33a537f4edef8
unknown <vinayak.jadhav7991@gmail.com>
2024-12-03 20:25:36 +0530

    fecth api with async and await

--- Ajax-and-api/app.js
+++ Ajax-and-api/app.js
@@ -1,21 +1,46 @@
+// let url = "https://catfact.ninja/fact";
+
+//   fetch(url)
+//     .then((res) => {
+//       return res.json();
+//     })
+//     .then((data) => {
+//       console.log(data.fact) ;
+//       return fetch(url);
+//     })
+//     .then((res) => {
+//       return res.json();
+//     })
+//     .then((data2) => {
+//         console.log(data2.fact) ;
+//     })
+//     .catch((err) => {
+//       console.log("Error ", err);
+//     });
+
 let url = "https://catfact.ninja/fact";
 
-  fetch(url)
-    .then((res) => {
-      return res.json();
-    })
-    .then((data) => {
-      console.log(data.fact) ;
-      return fetch(url);
-    })
-    .then((res) => {
-      return res.json();
-    })
-    .then((data2) => {
-        console.log(data2.fact) ;
-    })
-    .catch((err) => {
-      console.log("Error ", err);
-    });
+async function getData () {
+    try{
+        let res = await fetch(url);
+        if(!res.ok){
+            throw new Error(`Response status: ${response.status}`);
+        }
+        let data = await res.json();
+        console.log(data.fact);
+
+        let res2 = await fetch(url);
+        if(!res2.ok){
+            throw new Error(`Response status: ${response.status}`);
+        }
+        let data2 = await res2.json();
+        console.log(data.fact);
+    }catch(err) {
+        console.log(err);
+    }
+}
+
+
+
 
 
