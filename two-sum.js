5a59000cb25d2c530dd28ad0b7144041522ae948
unknown <vinayak.jadhav7991@gmail.com>
2025-07-23 15:07:04 +0530

    modified two sum algo

--- two-sum.js
+++ two-sum.js
@@ -10,8 +10,18 @@ function twoSum(arr, target) {
 
 console.log(twoSum([2, 4, 7, 9,6,1], 10));
 
+function twoSum2(arr, target) {
+    let newMap = new Map();
+    for (let i = 0; i < arr.length; i++) {
+        let complement = target - arr[i];
+        if (newMap.has(complement)) {
+            return [newMap.get(complement), i];
+        }
+        newMap.set(arr[i], i);
+    }
+}
 
-
+console.log(twoSum2([2, 4, 7, 9,6,1], 10));
 
 // let arr = [1, 2, 3, 4, 5, 6];
 // target = 10;
