29e0b6c539162761f3627f487b311f5d49e5add0
Vinayak Jadhav <vjadhav1010@gmail.com>
2025-10-13 01:36:17 +0530

    number of factors function in Java

---
+++ Java/numberOfFactors.java
@@ -0,0 +1,19 @@
+public class numberOfFactors{
+    public static int factors (int N){
+        int count = 0;
+        if(N == 0) return 0;
+        for(int i=1; i*i<N; i++){
+            if(N % i == 0){
+                if(N/i == i){
+                    count++;
+                }else{
+                    count+=2;
+                }
+            }
+        }
+        return count;
+    }
+    public static void main(String args[]){
+        System.out.print(factors(10));
+    }
+}
