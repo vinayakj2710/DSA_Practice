8aa47119a3d57af0f5f6d82334f0326fcd6a7753
Vinayak Jadhav <vjadhav1010@gmail.com>
2025-10-13 02:33:49 +0530

    bug - fix

--- Java/numberOfFactors.java
+++ Java/numberOfFactors.java
@@ -2,7 +2,7 @@ public class numberOfFactors{
     public static int factors (int N){
         int count = 0;
         if(N == 0) return 0;
-        for(int i=1; i*i<N; i++){
+        for(int i=1; i*i<=N; i++){
             if(N % i == 0){
                 if(N/i == i){
                     count++;
@@ -14,6 +14,6 @@ public class numberOfFactors{
         return count;
     }
     public static void main(String args[]){
-        System.out.print(factors(10));
+        System.out.print(factors(49));
     }
 }
