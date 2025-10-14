cbce52b7b35c40248268bf176586165f171ac871
Vinayak Jadhav <vjadhav1010@gmail.com>
2025-10-14 06:08:52 +0530

    Prob - Rotate an array K times

--- Java/reverseArray.java
+++ Java/reverseArray.java
@@ -1,6 +1,6 @@
 public class reverseArray{
 
-    public static int[] reverseArray(int[] A){
+    public static int[] reverseArray(int[] A){  
         int N = A.length;
         int i = 0;
         int j = N-1;
@@ -13,12 +13,59 @@ public class reverseArray{
         }
         return A;
     }
+    /* T.C = O(N) --- S.C = O(1) */
+
+//-----------Reverse Part of an Array---------------------------//
+
+    public static int[] revPartArray (int[] A, int si, int ei){  
+        // si - Start index, ei - End index
+        int N = A.length;
+        while(si < ei){
+            int temp = A[si];
+            A[si] = A[ei];
+            A[ei] = temp;
+            si++;
+            ei--;
+        }
+        return A;
+    }
+    /* T.C = O(N) --- S.C = O(1) */
+
+//---------------- Rotate an array K times ----------------------------------//
+
+    public static int[] reverseKtimes(int[] A, int K){
+        int N = A.length;
+        K = K % N;
+        revPartArray(A, 0, N-1);
+        revPartArray(A, 0, K-1);
+        revPartArray(A, K, N-1);
+        return A;
+    }
+    /* T.C = O(N) --- S.C = O(1) */
+
     public static void main(String args[]){
         int[] A = {1,2,3,4};
+        int[] B = {5,6,7,8};
+        int[] C = {9,10,11,12};
+
         int[] result = reverseArray(A);
 
         for(int res : result){
             System.out.print(res+" ");
+        }
+        System.out.println("");
+
+        int[] result2 = revPartArray(B, 1, 2);
+
+        for(int res2 : result2){
+            System.out.print(res2+" ");
+        }
+        System.out.println("");
+
+        int[] result3 = reverseKtimes(C, 2);
+
+        for(int res3 : result3){
+            System.out.print(res3+" ");
         }
     }
 }
--- reverseArray.js
+++ reverseArray.js
@@ -11,6 +11,34 @@ function reverseArray(arr) {
   return arr;
 }
 
-console.log(reverseArray([1, 2, 3, 4]));
+console.log(reverseArray([1, 2, 3, 4])); /* T.C = O(N) --- S.C = O(1) */
 
-/* T.C = O(N) --- S.C = O(1) */
+//-----------Reverse Part of an Array----------------------------------------//
+
+function revPartArray(arr, si, ei) {
+  // si - Start index, ei - End index
+  let N = arr.length;
+  while (si < ei) {
+    let temp = arr[si];
+    arr[si] = arr[ei];
+    arr[ei] = temp;
+    si++;
+    ei--;
+  }
+  return arr;
+}
+
+console.log(revPartArray([1, 2, 3, 4], 1, 2)); /* T.C = O(N) --- S.C = O(1) */
+
+//---------------- Rotate an array K times ----------------------------------//
+
+function rotateKtimes(arr, K) {
+  let N = arr.length;
+  K = K % N;
+  revPartArray(arr, 0, N - 1);
+  revPartArray(arr, 0, K - 1);
+  revPartArray(arr, K, N - 1);
+  return arr;
+}
+
+console.log(rotateKtimes([1, 2, 3, 4], 2)); /* T.C = O(N) --- S.C = O(1) */
